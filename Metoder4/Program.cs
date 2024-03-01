28bfd4236b08af6cc84f48daa5f4f0c94a7c0df2
baronvonpurr <robinhood.svensson@hotmail.com>
2024-03-01 09:16:46 +0100

    Add project files.

---
+++ Metoder4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Metoder4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in två tal");
+            double tal = double.Parse(Console.ReadLine());
+            double tal2 = double.Parse(Console.ReadLine());
+            Console.WriteLine(beräkning(tal, tal2));
+        }
+
+        static double beräkning(double tal, double tal2)
+        {
+            double tal3 = tal;
+            for (int i = 0; i < tal2 - 1; i++)
+            {
+                tal *= tal3;
+            }
+            return tal;
+        }
+    }
+}
